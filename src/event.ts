2fb94e8eaac84757b3e4f6ed55da7c2d03225b67
NUANCE\Fallon_Rohllf <fallon.rohllf@nuance.com>
2021-06-24 10:39:26 -0700

    Added event to get tag version

---
+++ src/event.ts
@@ -0,0 +1,16 @@
+import * as github from '@actions/github'
+import * as core from '@actions/core'
+
+export function getCreatedTag(): string | null{
+    if (github.context.eventName != 'create'){
+        core.info('The event namre was ${github.context.eventName}')
+        return null 
+    }
+
+    if (github.context.payload.ref_type != 'tag'){
+        core.info('The ecreate reference was a branch, not a tag')
+        return null
+    }
+
+    return github.context.payload.ref_type
+}
