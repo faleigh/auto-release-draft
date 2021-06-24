f36e049b96af94fe71baf47784b61422c4417c5a
NUANCE\Fallon_Rohllf <fallon.rohllf@nuance.com>
2021-06-24 13:17:18 -0700

    create draft from tag

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
 import * as core from '@actions/core'
 import * as event from './event'
 import * as version from './version'
+import * as git from './git'
+import * as github from './github'
+
 
 export async function run(): Promise<void> {
   try {
+    const token = core.getInput('repo-token')
     const tag = event.getCreatedTag()
+    var releaseUrl = ''
 
-    if (tag && version.isSemVer(tag)){
-      //TODO
+    if (tag && version.isSemVer(tag)) {
+      const changeLog = await git.getChangesIntroducedByTag(tag)
+      releaseUrl = await github.CreateReleaseDraft(tag, token, changeLog)
     }
 
-    core.setOutput('release-url', 'https://example.com')
+    core.setOutput('release-url', releaseUrl)
   } catch (error) {
     core.setFailed(error.message)
   }
