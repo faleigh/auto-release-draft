11a32bb3ab162d5d1f6a4bcfb0387a7c860b06b3
NUANCE\Fallon_Rohllf <fallon.rohllf@nuance.com>
2021-06-24 10:43:07 -0700

    Fix errors

--- src/event.ts
+++ src/event.ts
@@ -2,12 +2,12 @@ import * as github from '@actions/github'
 import * as core from '@actions/core'
 
 export function getCreatedTag(): string | null{
-    if (github.context.eventName != 'create'){
+    if (github.context.eventName !== 'create'){
         core.info('The event namre was ${github.context.eventName}')
         return null 
     }
 
-    if (github.context.payload.ref_type != 'tag'){
+    if (github.context.payload.ref_type !== 'tag'){
         core.info('The ecreate reference was a branch, not a tag')
         return null
     }
